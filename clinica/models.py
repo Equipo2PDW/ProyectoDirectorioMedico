afb34cb1ba8ef3c97590b3b155a742f4538f276f
debora-alayo <debora.alayo@sansano.usm.cl>
2023-09-20 20:46:42 -0300

    modelos medico

--- clinica/models.py
+++ clinica/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Especialidad(models.Model):
+    tipo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.tipo
+    
+    class Meta:
+        verbose_name_plural="Especialidades"
+        ordering=['tipo']
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural="Comunas"
+        ordering=['nombre']
+
+class Estudios(models.Model): #credenciales
+    medico = models.ForeignKey('Medico', on_delete=models.CASCADE)
+    nombre_programaestudio = models.CharField(max_length=100) #posgrado de cardiologia venas, etc
+    nombre_casaestudios = models.CharField(max_length=200) #universidad, etc
+
+    def __str__(self):
+        return self.medico + " " + self.nombre_programaestudio 
+    class Meta:
+        verbose_name_plural="Estudios"
+        ordering=['medico']
+
+class Medico(models.Model): #antecedentes y credenciales
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    especialidades = models.ManyToManyField(Especialidad)
+    ubicaciones = models.ManyToManyField(Comuna)
+    anos_experiencia = models.IntegerField() #antecedentes
+
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+    
+    class Meta:
+        verbose_name_plural="Médicos"
+        ordering=['nombre']
