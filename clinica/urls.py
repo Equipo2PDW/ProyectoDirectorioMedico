79959a5fa111b7824e6dadceca1ea8bea4075d02
Joaquin Gatica <joaquin.gatica@usm.cl>
2023-09-21 14:28:36 -0300

    PPDS-40 #listo #comment Tarea lista, revisar en /clinica/1

--- clinica/urls.py
+++ clinica/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.urls import include
 
+from .views import detail_view
+
 urlpatterns = [
-    
+    path('<id>', detail_view),
 ]
--- clinica/views.py
+++ clinica/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Medico
+
+def detail_view(request, id):
+    context = {}
+    context['data'] = Medico.objects.get(id = id)
+
+    return render(request, "Detail_view.html", context)
