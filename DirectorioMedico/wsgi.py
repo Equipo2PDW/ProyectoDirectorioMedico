37ae68e77b4ba44fbeaad99486b29fd84595571f
Joaquin Gatica <joaquin.gatica@usm.cl>
2023-11-08 00:08:57 -0300

    fix root_url

--- DirectorioMedico/wsgi.py
+++ DirectorioMedico/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DirectorioMedico.settings')
+settings_module = "DirectorioMedico.settings.prod" if os.getenv('DJANGO_ENV') == 'prod' else "DirectorioMedico.settings.dev"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
