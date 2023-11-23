825de6f751b9cc1e6ad7a143e3654671e82accad
Joaquin Gatica <surrenderpls2001@gmail.com>
2023-11-23 20:17:45 -0300

    new try workflow tests

--- clinica/tests.py
+++ clinica/tests.py
@@ -47,12 +47,10 @@ class InicialTest(LiveServerTestCase):
 
 class CrearCitaTest(LiveServerTestCase):
 	fixtures = {'dump_data.json'}
-	chrome_options = webdriver.ChromeOptions()
-	chrome_options.binary_location = '/usr/bin/google-chrome'
 	 
 	def test_crear_cita_correcta(self):
 
-		driver = webdriver.Chrome(options=self.chrome_options)
+		driver = webdriver.Chrome()
 
 		driver.get(self.live_server_url + '/clinica/create_cita')
 
@@ -87,7 +85,7 @@ class CrearCitaTest(LiveServerTestCase):
 		driver.quit()
 
 	def test_crear_cita_hora_incorrecta(self):
-		driver = webdriver.Chrome(options=self.chrome_options)
+		driver = webdriver.Chrome()
 
 		driver.get(self.live_server_url + '/clinica/create_cita')
 
@@ -130,7 +128,7 @@ class CrearCitaTest(LiveServerTestCase):
 		driver.quit()
 	
 	def test_crear_cita_fecha_incorrecta(self):
-		driver = webdriver.Chrome(options=self.chrome_options)
+		driver = webdriver.Chrome()
 
 		driver.get(self.live_server_url + '/clinica/create_cita')
 
